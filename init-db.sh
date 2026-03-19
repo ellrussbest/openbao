0fe66231471d22d377c840ab680e2e499dfb9197
Odhiambo Russell <72700083+ellrussbest@users.noreply.github.com>
2026-03-19 14:49:10 +0300

    Update init-db.sh for database creation and ownership
    Modified init-db.sh to create the database if it doesn't exist and ensure correct ownership.

--- init-db.sh
+++ init-db.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER bao WITH PASSWORD '$POSTGRES_PASSWORD';
-    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO bao;
+psql -v ON_ERROR_STOP=1 \
+  --username "$POSTGRES_USER" \
+  --dbname "postgres" <<-EOSQL
+
+-- Create database if it doesn't exist
+SELECT 'CREATE DATABASE ' || quote_ident('$POSTGRES_DB')
+WHERE NOT EXISTS (
+  SELECT FROM pg_database WHERE datname = '$POSTGRES_DB'
+)\gexec
+
+-- Ensure correct ownership
+ALTER DATABASE "$POSTGRES_DB" OWNER TO "$POSTGRES_USER";
+
 EOSQL
